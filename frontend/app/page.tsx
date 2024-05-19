f90c99be0c2fd2d0afe55e89040823d1a4e7c53a
snowgoose0 <jason2580134@outlook.com>
2024-05-18 21:19:41 -0700

    gg easy go next

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,26 @@
 "use client";
 import { signOut, signIn, useSession } from "next-auth/react";
+import { app, db } from "./firebase.ts"
+import { useCollection } from "react-firebase-hooks/firestore"
+import { getDocs, collection, query, where } from "firebase/firestore";
 
 export default function Home() {
     const session = useSession();
+    const fc = {
+        field: 'name',
+        operator: '==',
+        value: 'Jayson Yi'
+    }
+
+    console.log(session);
+    
+    // @ts-ignore 
+    const q = query(collection(db, "users"));
+    getDocs(q).then((qs) => {
+        qs.forEach((doc)=> {
+            console.log(doc.data().name);
+        });
+    });
 
     console.log(session);
 
--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -7,15 +7,15 @@ export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
         }),
     ],
     adapter: FirestoreAdapter({
         credential: cert({
-            projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
-            clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY.replace(
+            projectId: process.env.NEXT_PUBLIC_AUTH_FIREBASE_PROJECT_ID,
+            clientEmail: process.env.NEXT_PUBLIC_AUTH_FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.NEXT_PUBLIC_AUTH_FIREBASE_PRIVATE_KEY.replace(
                 /\\n/g,
                 "\n"
             ),
