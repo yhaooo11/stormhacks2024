09b18be1ee68cbd00c2205f5e463b3c95a321513
HubertTheodore <hubertotheo@gmail.com>
2024-05-19 05:10:09 -0700

    added background colour to researcher and participant signin

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Login() {
     return (
         <div className="flex flex-row justify-center items-center w-full h-full gap-14">
-            <a className="h-200 w-300" href="">
+            <a className="bg-[#FFDBB4] px-10 py-5 h-200 w-300" href="">
                 <Image
                     src="/assets/Researcher_Image.svg"
                     width={120*3}
@@ -16,7 +16,7 @@ export default function Login() {
                 <p className="font-bold text-center">Researcher</p>
             </a>
             <h2 className="font-bold">OR</h2>
-            <a className="h-200 w-300" href="">
+            <a className="bg-[#FFDBB4] px-10 py-5 h-200 w-300" href="">
                 <Image
                     src="/assets/Participant_Image.svg"
                     width={100*3}
